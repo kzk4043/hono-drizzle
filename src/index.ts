f0f4953f4d39de490b4f671f63296bb4b88eaab4
kzk4043 <okazakishoma@team-lab.com>
2024-08-19 09:31:34 +0900

    feat: honoでdrizzle使う

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { db } from "./db/db";
+import { todos } from "./db/schema";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.get("/api/todos", (c) => {
+  const allTodo = db.select().from(todos).all();
+  return c.json({ allTodo });
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
